78f85e8a6f8bd8e754c7f9e1afa13bca9c2ebc86
z <z@xnz.me>
2015-12-03 23:02:11 -0500

    table improvements

--- js/app.js
+++ js/app.js
@@ -4,30 +4,70 @@ $(document).ready(function() {
         "lengthMenu": [[50, 100, 250, 500], [50, 100, 250, 500]],
         "pageLength": 100,
         "columns": [
+            { "data": "pk3" },
+            { "data": "bsp" },
+            { "data": "filesize" },
+            { "data": "shasum" },
             { "data": "title" },
             { "data": "author" },
-            { "data": "bsp" },
+            { "data": "mapshot" },
             { "data": "map" },
             { "data": "radar" },
-            { "data": "waypoints" },
-            { "data": "shasum" },
-            { "data": "pk3" }
+            { "data": "waypoints" }
         ],
         "columnDefs": [
-            {
-                "targets": 4,
-                "render": function ( data, type, full, meta ) {
-                    return (data != false) ? true : false;
-                }
-            },
-            {
-                "targets": 7,
+            {   // pk3
+                "targets": 0,
                 "render": function ( data, type, full, meta ) {
                   return type === 'display' && data.length > 40 ?
                     '<span title="'+data+'">'+data.substr( 0, 38 )+'...</span>' :
                     data;
                 }
+            },
+            {   // pk3
+                "targets": 1,
+                "render": function ( data, type, full, meta ) {
+                    return data.replace('maps/','');
+                }
+            },
+            {   // filesize
+                "targets": 2,
+                "render": function ( data, type, full, meta ) {
+                    return(bytesToSize(data));
+                }
+            },
+            {   // mapshot file
+                "targets": 5,
+                "render": function ( data, type, full, meta ) {
+                    return (data != false) ? true : false;
+                }
+            },
+            {   // map file
+                "targets": 6,
+                "render": function ( data, type, full, meta ) {
+                    return (data != false) ? true : false;
+                }
+            },
+            {   // radar file
+                "targets": 7,
+                "render": function ( data, type, full, meta ) {
+                    return (data != false) ? true : false;
+                }
+            },
+            {   // waypoints file
+                "targets": 8,
+                "render": function ( data, type, full, meta ) {
+                    return (data != false) ? true : false;
+                }
             }
         ]
     } );
+
 } );
+
+function bytesToSize(bytes) {
+   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+   if (bytes == 0) return '0 Byte';
+   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+};
