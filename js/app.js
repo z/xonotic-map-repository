e2fb4c49d36d1b4ddfd6cd123964a88bfb6238e4
z <z@xnz.me>
2015-12-06 14:40:24 -0500

    had to put the drawcallback back

--- js/app.js
+++ js/app.js
@@ -168,6 +168,8 @@ $(document).ready(function() {
             $("#table-maplist_length").addClass("pull-right");
             $("#table-maplist_filter").addClass("pull-right");
             $("#table-controls").detach().appendTo('#nav-table-controls');
+        },
+        "drawCallback": function( settings ) {
             initPopovers();
         }
     } );
