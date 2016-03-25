e874aef2cb5a191eeff3d830f71d69021fee1242
z <z@xnz.me>
2016-03-24 20:44:53 -0400

    oh ya, this hacky file too

--- bin/ent2json.py
+++ bin/ent2json.py
@@ -5,11 +5,11 @@
 import json
 import re
 
-
 def replace_last(s, old, new):
     return s[::-1].replace(old[::-1], new[::-1], 1)[::-1]
 
 in_file = 'resources/entities/gasoline_02.ent'
+#in_file = 'resources/entities/vapor_alpha_2.ent'
 out_file = 'test.json'
 
 f = open(in_file, 'r')
@@ -62,19 +62,22 @@ from matplotlib import pyplot as plt
 import numpy as np
 import matplotlib
 
+#radar_image = 'resources/radars/gfx/vapor_alpha_2_mini.png'
 #radar_image = 'resources/radars/gfx/gasoline_02_mini.png'
-radar_image = 'space-elevator_radar.png'
+radar_image = 'gasoline_02_radar.png'
+#radar_image = 'space-elevator_radar.png'
 #radar_image = 'stinkbug.png'
 
-fig, ax = plt.subplots()
-
+fig = plt.figure(figsize=(6, 6), dpi=96)
+#fig = plt.figure()
+ax = fig.add_subplot(111)
 plt.axis('off')
 
 
 def plot_it(zipped, marker, color):
 
     x, y = zip(*zipped)
-    s = [100]
+    s = [10]
 
     plt.scatter(x, y, s, c=color, alpha=0.5, marker=marker)
 
@@ -93,32 +96,50 @@ if entities_ammo:
 if entities_spawn:
     plot_it(entities_spawn, 'o', "w")
 
+print(entities_health)
+
 # Remove margins
 plot_margin = 0
-x0, x1, y0, y1 = plt.axis()
+#x0, x1, y0, y1 = plt.axis()
+
+# add image
+img = plt.imread(radar_image)
+
+x0 = -1544
+x1 = 1544
+y0 = -3592
+y1 = 6664
+
 plt.axis((x0 - plot_margin,
           x1 + plot_margin,
           y0 - plot_margin,
           y1 + plot_margin))
 plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
 
-# add image
-img = plt.imread(radar_image)
+aspect = max(abs(x0) + abs(x1), abs(y0) + abs(y1)) / 512
+
+print(aspect)
 
 x0, x1 = ax.get_xlim()
 y0, y1 = ax.get_ylim()
-#ax.imshow(img, extent=[x0, x1, y0, y1], origin='lower')
-ax.imshow(img, extent=[x0, x1, y0, y1], origin='lower')
 
+#ax.imshow(img, extent=[x0, x1, y0, y1])
 
+longest = max(abs(x0) + abs(x1), abs(y0) + abs(y1)) / 2
+
+#ax.imshow(img, extent=[-longest, longest, -longest, longest])
+#plt.imshow(img, extent=[-longest, longest, -longest, longest])
+#ax.imshow(img, extent=[y0, y1, x0, x1], origin='upper')
+#plt.imshow(img)
+
+plt.axes().set_aspect('equal', 'datalim')
 plt.show()
 fig.canvas.draw()
 
 
-plt.savefig("test.png")
+plt.savefig("test.png", transparent=True)
 
 
-
-f = open(out_file, 'w')
-f.write(json.dumps(entities))
-f.close()
+#f = open(out_file, 'w')
+#f.write(json.dumps(entities))
+#f.close()
